59a424d3cb22923de321166de69e69440b264a87
Argentumbolo <argentumbolo@yandex.ua>
2014-11-20 23:38:30 +0200

    tmp

--- src/db.py
+++ src/db.py
@@ -6,13 +6,14 @@ def _create_all():
     global _db
     _db.execute("CREATE TABLE IF NOT EXISTS [percents] ("
                 "    [day_of_entry]     DATE NOT NULL,"
-                "    [from_sum]         REAL NOT NULL,"
+                "    [from_amount]      REAL NOT NULL,"
                 "    [one_day_interest] REAL NOT NULL,"
                 "    PRIMARY KEY(day_of_entry, from_sum)"
                 ")")
     _db.execute("CREATE TABLE IF NOT EXISTS [deposits] ("
                 "    [id]                    INTEGER PRIMARY KEY ASC,"
                 "    [account]               TEXT UNIQUE NOT NULL,"
+                "    [create_date]           DATE NOT NULL,"
                 "    [balance]               REAL NOT NULL," # REAL for simplicity, look [strategy of rounding].
                 "    [capitalization_period] INTEGER"        # NULL: withot capitalization; 0: monthly; >0: period in days.
                 ")")
@@ -22,18 +23,20 @@ def _create_all():
                 "    [amount] REAL,"
                 "    FOREIGN KEY(account_id) REFERENCES [deposits](id)"
                 ")")
-    
 
 def connect(dbname):
     global _db
     _db = sqlite3.connect(dbname)
+    _create_all()
 
 def close():
     global _db
     _db.close()
 
+def get_interest_amount(date, balance):
+    global _db
+    _db.execute("SELECT day_of_entry as doe, one_day_interest as odi GROUP BY ")
+    
+
 connect("a.dbt")
-_create_all()
 close()
-
-
