6711ae2108141bde713af96f6bf2fa4cf4e14908
Argentumbolo <argentumbolo@yandex.ua>
2014-11-24 22:26:20 +0200

    Modify db. Add main.

--- src/db.py
+++ src/db.py
@@ -4,24 +4,24 @@ _db = None
 
 def _create_all():
     global _db
-    _db.execute("CREATE TABLE IF NOT EXISTS [percents] ("
-                "    [day_of_entry]     DATE NOT NULL,"
-                "    [from_amount]      REAL NOT NULL,"
-                "    [one_day_interest] REAL NOT NULL,"
-                "    PRIMARY KEY(day_of_entry, from_sum)"
+    _db.execute("CREATE TABLE IF NOT EXISTS [interest_plan] (\n"
+                "    [day_of_entry]     DATE NOT NULL,\n"
+                "    [from_amount]      REAL NOT NULL,\n"
+                "    [one_day_interest] REAL NOT NULL,\n"
+                "    PRIMARY KEY(day_of_entry, from_sum)\n"
                 ")")
-    _db.execute("CREATE TABLE IF NOT EXISTS [deposits] ("
-                "    [id]                    INTEGER PRIMARY KEY ASC,"
-                "    [account]               TEXT UNIQUE NOT NULL,"
-                "    [create_date]           DATE NOT NULL,"
-                "    [balance]               REAL NOT NULL," # REAL for simplicity, look [strategy of rounding].
-                "    [capitalization_period] INTEGER"        # NULL: withot capitalization; 0: monthly; >0: period in days.
+    _db.execute("CREATE TABLE IF NOT EXISTS [deposits] (\n"
+                "    [id]                    INTEGER PRIMARY KEY ASC,\n"
+                "    [account]               TEXT UNIQUE NOT NULL,\n"
+                "    [create_date]           DATE NOT NULL,\n"
+                "    [balance]               REAL NOT NULL,\n" # REAL for simplicity, look [strategy of rounding].
+                "    [capitalization_period] INTEGER\n"        # NULL: withot capitalization; 0: monthly; >0: period in days.
                 ")")
-    _db.execute("CREATE TABLE IF NOT EXISTS [interest] (    "
-                "    [id] INTEGER PRIMARY KEY ASC,"
-                "    [account_id] INTEGER NOT NULL,"
-                "    [amount] REAL,"
-                "    FOREIGN KEY(account_id) REFERENCES [deposits](id)"
+    _db.execute("CREATE TABLE IF NOT EXISTS [interest] (\n"
+                "    [id]         INTEGER PRIMARY KEY ASC,\n"
+                "    [account_id] INTEGER NOT NULL,\n"
+                "    [amount]     REAL,\n"
+                "    FOREIGN KEY(account_id) REFERENCES [deposits](id)\n"
                 ")")
 
 def connect(dbname):
@@ -33,10 +33,25 @@ def close():
     global _db
     _db.close()
 
+def add_interest_plan_point(day_of_entry, from_amount, one_day_interest):
+    global _db
+    _db.execute("INSERT OR REPLACE\n"
+                "INTO [interest_plan] (day_of_entry, from_amount, one_day_interest)\n"
+                "VALUES (?, ?, ?)",
+                (day_of_entry, from_amount, one_day_interest))
+
 def get_interest_amount(date, balance):
     global _db
-    _db.execute("SELECT day_of_entry as doe, one_day_interest as odi GROUP BY ")
-    
+    interest = _db.execute("SELECT PFA\n"
+                           "FROM ( SELECT [interest_plan].day_of_entry as PDE, [interest_plan].from_amount as PFA\n"
+                           "       FROM [interest_plan]\n"
+                           "       WHERE PDE <= :date AND PFA <= :balance\n"
+                           "       GROUP BY PFA\n"
+                           "       ORDER BY PDE ASC\n"
+                           "     )\n"
+                           "ORDER BY PFA DESC\n"
+                           "LIMIT 1",
+                           date = date, balance = balance)[0][0]
 
-connect("a.dbt")
-close()
+#connect("a.dbt")
+#close()
---
+++ src/main.py
@@ -0,0 +1,8 @@
+import db
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    db.connect("a.dbt")
+    main()
